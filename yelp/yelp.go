6cd9f0184cbcb31a8159f11c68c27480b4f720ec
JC Ivancevich <jc.ivancevich@gmail.com>
2017-12-22 18:24:08 -0300

    use api key instead of access token

--- yelp/yelp.go
+++ yelp/yelp.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"time"
 )
 
 const (
@@ -20,9 +19,6 @@ const (
 
 	// businessPath is the path to get a business by its id
 	businessPath = "/v3/businesses/%s"
-
-	// tokenPath is the path to fetch the bearer's access token
-	tokenPath = "/oauth2/token"
 )
 
 // Client defines the current available Yelp API requests that can be made.
@@ -34,33 +30,15 @@ type Client interface {
 // client implements the Client interface.
 type client struct {
 	*http.Client
-	credentials Credentials
+	apiKey string
 }
 
 // New returns a new Yelp client.
-func New(c *http.Client, credentials Credentials) *client {
+func New(c *http.Client, apiKey string) *client {
 	return &client{
-		Client:      c,
-		credentials: credentials,
+		Client: c,
+		apiKey: apiKey,
 	}
-}
-
-// FetchToken makes a post request to get the auth token for credentials passed
-// to the client on initialization.
-func (c *client) FetchToken() error {
-	respBody := struct {
-		AccessToken string `json:"access_token"`
-		TokenType   string `json:"token_type"`
-		ExpiresIn   int64  `json:"expires_in"`
-	}{}
-	_, err := c.postForm(apiHost+tokenPath, c.credentials.URLValues(), &respBody)
-	if err != nil {
-		return err
-	}
-
-	c.credentials.AccessToken = respBody.AccessToken
-	c.credentials.ExpiryDate = time.Now().Add(time.Duration(respBody.ExpiresIn) * time.Second)
-	return nil
 }
 
 // Search makes a request given the options passed in.
@@ -88,12 +66,6 @@ func (c *client) BusinessByID(businessID string) (Business, error) {
 // request with the Authorization Header set with the access token. The response
 // body is decoded into v.
 func (c *client) authedDo(method string, url string, body io.Reader, headers map[string]string, v interface{}) (*http.Response, error) {
-	if !c.credentials.IsValid() {
-		if err := c.FetchToken(); err != nil {
-			return nil, err
-		}
-	}
-
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
@@ -102,7 +74,7 @@ func (c *client) authedDo(method string, url string, body io.Reader, headers map
 	for key, val := range headers {
 		req.Header.Set(key, val)
 	}
-	req.Header.Set("Authorization", "Bearer "+c.credentials.AccessToken)
+	req.Header.Set("Authorization", "Bearer "+c.apiKey)
 
 	resp, err := c.Do(req)
 	if err != nil {
